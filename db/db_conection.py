30b55d77f524774d394a0b2b9f69a794a3f75264
XantX <hp_xantx@hotmail.com>
2023-03-23 21:08:36 -0500

    Configuracion de puerto para conexion a base de datos sql

--- db/db_conection.py
+++ db/db_conection.py
@@ -5,6 +5,7 @@ from os import getenv
 class DataBase:
     def __init__(self):
         self.connection = pymysql.connect(
+                port=int(getenv('PORT')),
                 host=getenv('HOST'),
                 user=getenv('USER'),
                 password=getenv('PASS'),
